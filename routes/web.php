1bb20eef15e7aa2da5454e885339f5fbc83a82b8
2301110451 <153810938+2301110451@users.noreply.github.com>
2026-04-26 00:13:48 +0800

    Add ReleaseChangelogTestController to routes

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\Central\UpdateAnnouncementController;
 use App\Http\Controllers\Central\FeatureLabController;
 use App\Http\Controllers\Central\ReleaseSmokeTestController; // ✅ NEW
 use App\Http\Controllers\Central\ReleaseFlowTestController;
+use App\Http\Controllers\Central\ReleaseChangelogTestController;
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
